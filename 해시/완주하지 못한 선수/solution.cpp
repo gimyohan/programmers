5493c8926f7a25795c532b3a47fc9fed063daa60
javago <135432909+gimyohan@users.noreply.github.com>
2025-03-31 19:19:24 +0900

    Update solution.cpp

--- 해시/완주하지 못한 선수/solution.cpp
+++ 해시/완주하지 못한 선수/solution.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-    multiset<string> mst;
-    for(auto &s: participant)mst.insert(s);
-    for(auto &s: completion)mst.erase(mst.find(s));
-    return *mst.begin();
+    multiset<string> mts;
+    for(auto &s: participant)mts.insert(s);
+    for(auto &s: completion)mts.erase(mts.find(s));
+    return *mts.begin();
 }
