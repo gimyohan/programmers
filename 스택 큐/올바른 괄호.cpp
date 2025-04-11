ca5c31482f2713389524d7f08626134018b24573
javago <135432909+gimyohan@users.noreply.github.com>
2025-04-11 22:19:29 +0900

    Create 올바른 괄호.cpp

---
+++ 스택 큐/올바른 괄호.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool solution(string s)
+{
+    int cnt = 0;
+    for(auto c:s){
+        if(c=='(')cnt++;
+        else cnt--;
+        if(cnt<0)return false;
+    }
+
+    return cnt==0;
+}
